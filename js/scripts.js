49f5bb7b312c8b964b6972ce16eb2ca1587bab18
Tristan Emmerson <tristan@stickerslug.com>
2020-06-19 10:25:21 -0700

    add comment notes to scripts.js for constructing business logic.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//BUSINESS LOGIC
+
+//quesadilla price elements (spicy/meat/vegetarian)
+
+//name input elements (first middle last)
+
+//empty arrays for push customer choices to (quesadillas/names)
+
+//prototype price changes relations (+=5/-=2)
+
+//prototype total price ($5.00)
+
+//DOM print quesadilla order details (your quesadilla price)
+
+//DOM print user name input details (your first middle last)
+
